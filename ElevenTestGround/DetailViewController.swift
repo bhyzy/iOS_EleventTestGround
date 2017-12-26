b72e91af0bba7935738ad2877eac926d097e9b85
Bartlomiej Hyzy <bartlomiej.hyzy@avanade.com>
2017-12-26 19:15:54 +0100

    Comments view

--- ElevenTestGround/DetailViewController.swift
+++ ElevenTestGround/DetailViewController.swift
@@ -12,5 +12,24 @@ class DetailViewController: UIViewController {
 
     var detailItem: AnyObject? = nil
     
+    @IBOutlet var expandedConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet weak var commentsActionLabel: UILabel!
+    @IBOutlet weak var commentsHeaderLabel: UILabel!
+    
+    @IBAction func tappedCommentsSection(_ recognizer: UITapGestureRecognizer) {
+        guard recognizer.state == UIGestureRecognizerState.recognized else { return }
+
+        expandedConstraint.isActive = !expandedConstraint.isActive
+        UIView.animate(withDuration: 0.3,
+                       delay: 0,
+                       options: [UIViewAnimationOptions.curveEaseOut, UIViewAnimationOptions.beginFromCurrentState, UIViewAnimationOptions.allowUserInteraction],
+                       animations: {
+                        self.view.layoutIfNeeded()
+                        self.commentsActionLabel.alpha = self.expandedConstraint.isActive ? 0 : 1
+                        self.commentsHeaderLabel.alpha = self.expandedConstraint.isActive ? 1 : 0
+            }
+        )
+    }
 }
 
