88426b860390d5d24c78a776f00f4d231956cf0d
Bartlomiej Hyzy <bartlomiej.hyzy@avanade.com>
2017-12-28 15:05:41 +0100

    First go at animations

--- ElevenTestGround/DetailViewController.swift
+++ ElevenTestGround/DetailViewController.swift
@@ -9,27 +9,64 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    var detailItem: AnyObject? = nil
     
-    @IBOutlet var expandedConstraint: NSLayoutConstraint!
-    @IBOutlet weak var tapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet weak var commentsView: UIView!
+    @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var commentsActionLabel: UILabel!
     @IBOutlet weak var commentsHeaderLabel: UILabel!
     
+    @IBOutlet var expandedConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var tapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet weak var panGestureRecognizer: UIPanGestureRecognizer!
+    
+    var detailItem: AnyObject? = nil
+    
+    var progressWhenInterrupted: CGFloat = 0
+    var animator: UIViewPropertyAnimator! = nil
+    let transitionDuration: TimeInterval = 0.3
+    
+    var commentsScrollRange: CGFloat = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        commentsScrollRange = photoImageView.convert(commentsView.bounds.origin, from: commentsView).y
+    }
+    
     @IBAction func tappedCommentsSection(_ recognizer: UITapGestureRecognizer) {
         guard recognizer.state == UIGestureRecognizerState.recognized else { return }
+        animateTransitionIfNeeded(duration: transitionDuration)
+    }
 
+    @IBAction func pannedCommentsSection(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            animateTransitionIfNeeded(duration: transitionDuration)
+            animator.pauseAnimation()
+            progressWhenInterrupted = animator.fractionComplete
+        case .changed:
+            let translation = recognizer.translation(in: photoImageView)
+            animator.fractionComplete = progressWhenInterrupted + translation.y / commentsScrollRange
+        case .ended:
+            let timing = UICubicTimingParameters(animationCurve: .easeOut)
+            animator.continueAnimation(withTimingParameters: timing, durationFactor: 0)
+        default:
+            break
+        }
+    }
+    
+    func animateTransitionIfNeeded(duration: TimeInterval) {
+        guard animator == nil || animator.isRunning == false else { return }
+        
         expandedConstraint.isActive = !expandedConstraint.isActive
-        UIView.animate(withDuration: 0.3,
-                       delay: 0,
-                       options: [UIViewAnimationOptions.curveEaseOut, UIViewAnimationOptions.beginFromCurrentState, UIViewAnimationOptions.allowUserInteraction],
-                       animations: {
-                        self.view.layoutIfNeeded()
-                        self.commentsActionLabel.alpha = self.expandedConstraint.isActive ? 0 : 1
-                        self.commentsHeaderLabel.alpha = self.expandedConstraint.isActive ? 1 : 0
-            }
-        )
+        
+        animator = UIViewPropertyAnimator(duration: duration, curve: .easeOut, animations: {
+            self.view.layoutIfNeeded()
+            self.commentsActionLabel.alpha = self.expandedConstraint.isActive ? 0 : 1
+            self.commentsHeaderLabel.alpha = self.expandedConstraint.isActive ? 1 : 0
+            })
+        animator.startAnimation()
     }
 }
 
