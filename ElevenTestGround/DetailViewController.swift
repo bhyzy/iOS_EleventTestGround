e4d079a3846982877798271ca6272fc98cadc3f0
Bartlomiej Hyzy <bartlomiej.hyzy@avanade.com>
2017-12-28 17:09:28 +0100

    Improved interactive pan gesture

--- ElevenTestGround/DetailViewController.swift
+++ ElevenTestGround/DetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var commentsView: UIView!
     @IBOutlet weak var photoImageView: UIImageView!
@@ -22,51 +22,137 @@ class DetailViewController: UIViewController {
     
     var detailItem: AnyObject? = nil
     
+    let transitionDuration: TimeInterval = 0.5
+    let dampingRatio: CGFloat = 1
+    let swipeVelocityThreshold: CGFloat = 200
+    let releaseProgressThreshold: CGFloat = 0.3
+    
     var progressWhenInterrupted: CGFloat = 0
-    var animator: UIViewPropertyAnimator! = nil
-    let transitionDuration: TimeInterval = 0.3
+
+    var runningAnimators = [UIViewPropertyAnimator]()
     
-    var commentsScrollRange: CGFloat = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    enum State {
+        case Collaped
+        case Expanded
         
-        commentsScrollRange = photoImageView.convert(commentsView.bounds.origin, from: commentsView).y
+        @discardableResult mutating func toggle() -> State {
+            switch self {
+            case .Collaped:
+                self = .Expanded
+            case .Expanded:
+                self = .Collaped
+            }
+            return self
+        }
     }
+    
+    var currentState: State = .Collaped
     
     @IBAction func tappedCommentsSection(_ recognizer: UITapGestureRecognizer) {
         guard recognizer.state == UIGestureRecognizerState.recognized else { return }
-        animateTransitionIfNeeded(duration: transitionDuration)
+        toggleCurrentState()
+        animateOrReverseRunningTransition(state: currentState, duration: transitionDuration)
     }
 
     @IBAction func pannedCommentsSection(_ recognizer: UIPanGestureRecognizer) {
         switch recognizer.state {
         case .began:
-            animateTransitionIfNeeded(duration: transitionDuration)
-            animator.pauseAnimation()
-            progressWhenInterrupted = animator.fractionComplete
+            if runningAnimators.isEmpty {
+                toggleCurrentState()
+            }
+            startInteractiveTransition(state: currentState, duration: transitionDuration)
         case .changed:
             let translation = recognizer.translation(in: photoImageView)
-            animator.fractionComplete = progressWhenInterrupted + translation.y / commentsScrollRange
+            let verticalDelta = translation.y * (currentState == .Collaped ? 1 : -1)
+            let deltaFraction = verticalDelta / photoImageView.frame.height
+            updateInteractiveTransition(fractionDelta: deltaFraction)
         case .ended:
-            let timing = UICubicTimingParameters(animationCurve: .easeOut)
-            animator.continueAnimation(withTimingParameters: timing, durationFactor: 0)
+            var cancel: Bool
+            let velocity = recognizer.velocity(in: photoImageView)
+            if abs(velocity.y) >= swipeVelocityThreshold {
+                cancel = (currentState == .Expanded && velocity.y > 0) || (currentState == .Collaped && velocity.y < 0)
+            } else {
+                let fractionCompleted = runningAnimators[0].fractionComplete
+                cancel = fractionCompleted < releaseProgressThreshold
+            }
+            continueInteractiveTransition(cancel: cancel)
+            if cancel {
+                toggleCurrentState()
+            }
         default:
             break
         }
     }
-    
-    func animateTransitionIfNeeded(duration: TimeInterval) {
-        guard animator == nil || animator.isRunning == false else { return }
+
+    func animateTransitionIfNeeded(state: State, duration: TimeInterval) {
+        guard runningAnimators.isEmpty else { return }
         
-        expandedConstraint.isActive = !expandedConstraint.isActive
-        
-        animator = UIViewPropertyAnimator(duration: duration, curve: .easeOut, animations: {
+        let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: dampingRatio) {
+            self.expandedConstraint.isActive = (state == .Expanded)
+            self.view.setNeedsLayout()
             self.view.layoutIfNeeded()
-            self.commentsActionLabel.alpha = self.expandedConstraint.isActive ? 0 : 1
-            self.commentsHeaderLabel.alpha = self.expandedConstraint.isActive ? 1 : 0
-            })
+        }
+        appendAnimator(frameAnimator)
+        
+        let labelAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: dampingRatio) {
+            self.commentsActionLabel.alpha = state == .Collaped ? 1 : 0
+            self.commentsHeaderLabel.alpha = state == .Expanded ? 1 : 0
+        }
+        appendAnimator(labelAnimator)
+    }
+    
+    func animateOrReverseRunningTransition(state: State, duration: TimeInterval) {
+        if runningAnimators.isEmpty {
+            animateTransitionIfNeeded(state: state, duration: duration)
+        } else {
+            reverseRunningAnimators()
+        }
+    }
+    
+    func startInteractiveTransition(state: State, duration: TimeInterval) {
+        animateTransitionIfNeeded(state: state, duration: duration)
+        for animator in runningAnimators {
+            animator.pauseAnimation()
+        }
+        progressWhenInterrupted = runningAnimators[0].fractionComplete
+    }
+    
+    func updateInteractiveTransition(fractionDelta: CGFloat) {
+        let fractionComplete = progressWhenInterrupted + fractionDelta
+        for animator in runningAnimators {
+            animator.fractionComplete = fractionComplete
+        }
+    }
+    
+    func continueInteractiveTransition(cancel: Bool) {
+        if cancel {
+            reverseRunningAnimators()
+        }
+        let timing = UISpringTimingParameters(dampingRatio: dampingRatio)
+        for animator in runningAnimators {
+            animator.continueAnimation(withTimingParameters: timing, durationFactor: 0)
+        }
+    }
+    
+    func appendAnimator(_ animator: UIViewPropertyAnimator) {
+        animator.addCompletion { _ in
+            if let index = self.runningAnimators.index(of: animator) {
+                self.runningAnimators.remove(at: index)
+            }
+        }
         animator.startAnimation()
+        runningAnimators.append(animator)
+    }
+    
+    func reverseRunningAnimators() {
+        for animator in runningAnimators {
+            animator.isReversed = !animator.isReversed
+        }
+    }
+    
+    func toggleCurrentState() {
+        currentState.toggle()
+        tapGestureRecognizer.isEnabled = currentState == .Collaped
     }
 }
 
